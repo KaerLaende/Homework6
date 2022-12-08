102592cb6c2a0843addce31fa63f627ab0b0d96b
Kaerlaende <kaerlaende@mail.ru>
2022-12-08 13:58:39 +0300

    Домашка №6 Циклы

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //Циклы.Часть 1
+        System.out.println("Задание 1");
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        //Задание 2
+        System.out.println("Задание 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        //Задание 3
+        System.out.println("Задание 3");
+        for (int i = 2; i < 17; i=i+2) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        //Задание 4
+        System.out.println("Задание 4");
+        for (int i = 10; i > -11; i--) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+}
